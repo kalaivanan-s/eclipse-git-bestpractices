7ee5104b703ebc23e20813dc0e08a614a190733f
S.Kalaivanan <kalaivanans@hotmail.com>
2023-02-20 09:27:38 +0530

    addded logger factory

--- src/main/java/com/javatechie/ProductServiceApplication.java
+++ src/main/java/com/javatechie/ProductServiceApplication.java
@@ -1,5 +1,7 @@
 package com.javatechie;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableCaching
 @ComponentScan(basePackages = "com.*")
 public class ProductServiceApplication {
+	
+	Logger loggger= LoggerFactory.getLogger(SpringApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
